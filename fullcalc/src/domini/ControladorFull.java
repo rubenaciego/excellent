3248c7920b633cae190c9dc193890c4a1758008d
FPifa <pifarre.esquerda@gmail.com>
2022-04-20 14:44:07 +0200

    Primeres definicions a Operador i Controlador Full

--- fullcalc/src/domini/ControladorFull.java
+++ fullcalc/src/domini/ControladorFull.java
@@ -1,5 +1,78 @@
 package domini;
 
 public class ControladorFull {
+    private Full full;
+    private Operador op;
 
+    public void executaOperacio(ResultatParserFull parsejat) {
+        if (parsejat.getTipusOpFull() == opFull.extreuHoroscop) {
+            MatriuCeles bloc = obteBloc(parsejat);
+            MatriuCeles res = op.extreuHorocop(bloc);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.extreuAny) {
+            MatriuCeles bloc = obteBloc(parsejat);
+            MatriuCeles res = op.extreuAny(bloc);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.extreuDia) {
+            MatriuCeles bloc = obteBloc(parsejat);
+            MatriuCeles res = op.extreuDia(bloc);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.extreuDiaSetmana) {
+            MatriuCeles bloc = obteBloc(parsejat);
+            MatriuCeles res = op.extreuDiaSetmana(bloc);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.executaOperacioAritmeticaUnaria) {
+            MatriuCeles bloc = obteBloc(parsejat);
+            MatriuCeles res = op.executaOperacioAritmeticaUnaria(bloc,
+                    parsejat.getTipusOpAritmetica());
+        }
+        else if (parsejat.getTipusOpFull() == opFull.executaFuncioEstadistica) {
+            MatriuCeles bloc = obteBloc(parsejat);
+            MatriuCeles res = op.executaFuncioEstadistica(bloc,
+                    parsejat.getTipusOpEstadistica());
+        }
+
+        else if (parsejat.getTipusOpFull() == opFull.truncaNumero) {
+            MatriuCeles bloc = obteBloc(parsejat);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.converteixUnitats) {
+            MatriuCeles bloc = obteBloc(parsejat);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.cercaOcurrencies) {
+            MatriuCeles bloc = obteBloc(parsejat);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.converteixMajuscules) {
+            MatriuCeles bloc = obteBloc(parsejat);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.converteixMinuscules) {
+            MatriuCeles bloc = obteBloc(parsejat);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.transposa) {
+            MatriuCeles bloc = obteBloc(parsejat);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.reemplaca) {
+            MatriuCeles bloc = obteBloc(parsejat);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.ordena) {
+            MatriuCeles bloc = obteBloc(parsejat);
+        }
+        else if (parsejat.getTipusOpFull() == opFull.modificaCela) {}
+        else if (parsejat.getTipusOpFull() == opFull.afegeixColumna) {}
+        else if (parsejat.getTipusOpFull() == opFull.afegeixFila) {}
+        else if (parsejat.getTipusOpFull() == opFull.eliminaColumna) {}
+        else if (parsejat.getTipusOpFull() == opFull.eliminaFila) {}
+        else if (parsejat.getTipusOpFull() == opFull.mouBloc) {}
+        else if (parsejat.getTipusOpFull() == opFull.copiaBloc) {}
+        else if (parsejat.getTipusOpFull() == opFull.buidaBloc) {}
+    }
+
+    private MatriuCeles obteBloc(ResultatParserFull parsejat) {
+        Integer filaIni = parsejat.getFilaOrigen();
+        Integer colIni = parsejat.getColumnaOrigen();
+        Integer numFiles = parsejat.getMidaFila();
+        Integer numCols = parsejat.getMidaColumna();
+        MatriuCeles bloc = full.obteBloc(filaIni, colIni, numFiles,
+                numCols);
+        return bloc;
+    }
 }
--- fullcalc/src/domini/Operador.java
+++ fullcalc/src/domini/Operador.java
@@ -1,6 +1,25 @@
 package domini;
 
-public class Operador
-{
-
+public class Operador {
+    public MatriuCeles extreuHorocop(MatriuCeles bloc) {}
+    public MatriuCeles extreuAny(MatriuCeles bloc) {}
+    public MatriuCeles extreuMes(MatriuCeles bloc) {}
+    public MatriuCeles extreuDia(MatriuCeles bloc) {}
+    public MatriuCeles extreuDiaSetmana(MatriuCeles bloc) {}
+    public MatriuCeles executaOperacioAritmeticaUnaria(MatriuCeles bloc,
+                                                       opAritmetica op) {}
+    public MatriuCeles executaFuncioEstadistica(MatriuCeles bloc,
+                                                opEstadistica op) {}
+    public MatriuCeles truncaNumero(MatriuCeles bloc, Integer n) {}
+    public MatriuCeles converteixUnitats(MatriuCeles bloc,
+                                         conversioUnitats conv) {}
+    public MatriuCeles extreuLongitudText(MatriuCeles bloc) {}
+    public MatriuCeles cercaOcurrencies(MatriuCeles bloc, String aCercar) {}
+    public MatriuCeles converteixMajuscules(MatriuCeles bloc) {}
+    public MatriuCeles converteixMinuscules(MatriuCeles bloc) {}
+    public MatriuCeles transposa(MatriuCeles bloc) {}
+    public MatriuCeles reemplaca(MatriuCeles bloc, String aCercar,
+                                 String aSubstituir) {}
+    public MatriuCeles ordena(MatriuCeles bloc, Integer col,
+                              criteriOrdenacio criteri) {}
 }
