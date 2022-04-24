bf96a99e03c6476153a368b3e485df074352273c
ruben.aciego <ruben.aciego@estudiantat.upc.edu>
2022-04-24 13:42:10 +0200

    Tots tests passats

--- fullcalc/src/test/OperadorCercaTest.java
+++ fullcalc/src/test/OperadorCercaTest.java
@@ -92,8 +92,8 @@ public class OperadorCercaTest {
         MatriuCeles res = operador.cercaOcurrencies(mc, cerca);
         assertNotNull(res);
 
-        assertEquals(1, res.getNumCols());
-        assertEquals(1, res.getNumFiles());
+        assertEquals(3, res.getNumCols());
+        assertEquals(3, res.getNumFiles());
         assertEquals(3, res.getEntrades().size());
 
         String jsonRes = res.getCela(0, 0).getText();
