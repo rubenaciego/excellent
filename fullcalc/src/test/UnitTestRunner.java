458d6ab95540eec6126c139227d6666940da4269
ruben.aciego <ruben.aciego@estudiantat.upc.edu>
2022-04-24 17:55:26 +0200

    UnitTestRunner

--- fullcalc/src/test/UnitTestRunner.java
+++ fullcalc/src/test/UnitTestRunner.java
@@ -4,7 +4,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-public class TestRunner {
+public class UnitTestRunner {
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(TotsTests.class);
 
