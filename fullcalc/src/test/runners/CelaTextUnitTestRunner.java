ece57244cc0ba172580047f9888e3e65f4532d5d
ruben.aciego <ruben.aciego@estudiantat.upc.edu>
2022-04-24 18:11:57 +0200

    Afegits test runners

--- fullcalc/src/test/runners/CelaTextUnitTestRunner.java
+++ fullcalc/src/test/runners/CelaTextUnitTestRunner.java
@@ -1,12 +1,14 @@
-package test;
+package test.runners;
 
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import test.CelaRefTest;
+import test.CelaTextTest;
 
-public class UnitTestRunner {
+public class CelaTextUnitTestRunner {
     public static void main(String[] args) {
-        Result result = JUnitCore.runClasses(TotsTests.class);
+        Result result = JUnitCore.runClasses(CelaTextTest.class);
 
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
